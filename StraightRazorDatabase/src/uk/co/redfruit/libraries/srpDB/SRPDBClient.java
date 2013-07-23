f1303482e1d1128cd49bcdd16e9e6ce5d826be46
Paul Hunnisett <paul@redfruit.co.uk>
2013-07-23 14:05:24 +0000

    cleaned up imports
    git-svn-id: https://redfruit.svn.cloudforge.com/straight_razor_database/StraightRazorDatabase/trunk@220 6addc35f-59c1-4424-aa4d-5cfc4df44541

--- StraightRazorDatabase/src/uk/co/redfruit/libraries/srpDB/SRPDBClient.java
+++ StraightRazorDatabase/src/uk/co/redfruit/libraries/srpDB/SRPDBClient.java
@@ -1,12 +1,6 @@
 package uk.co.redfruit.libraries.srpDB;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
