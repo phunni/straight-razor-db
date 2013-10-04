a9e0ec1177e556e1ee8619fa44c16e19c1530c43
Paul Hunnisett <paul@redfruit.co.uk>
2013-10-04 15:11:24 +0000

    Added new classes to represent content data so it can be more effectively displayed.
    git-svn-id: https://redfruit.svn.cloudforge.com/straight_razor_database/StraightRazorDatabase/trunk@295 6addc35f-59c1-4424-aa4d-5cfc4df44541

---
+++ StraightRazorDatabase/src/uk/co/monkeypower/android/straightrazordatabase/content/Content.java
@@ -0,0 +1,22 @@
+package uk.co.monkeypower.android.straightrazordatabase.content;
+
+public abstract class Content {
+	
+	private int pageID;
+	private String title;
+	public int getPageID() {
+		return pageID;
+	}
+	public void setPageID(int pageID) {
+		this.pageID = pageID;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+
+}
